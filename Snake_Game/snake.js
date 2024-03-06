c75189e67360f91ed7e37539c58041a105fc1922
Rayhan Al Shorif <126382033+rayhanalshorif133@users.noreply.github.com>
2024-03-06 11:08:29 +0600

    snake food

--- Snake_Game/snake.js
+++ Snake_Game/snake.js
@@ -1,25 +1,44 @@
 var direction = "";
+var position_top = 0;
+var position_left = 0;
+var position_right = 0;
+var position_bottom = 0;
+// any key
+document.addEventListener("keydown", function (event) {
+    direction = event.key;
+    moveSnake();
+}
+);
 
-console.log("Hello World");
+function moveSnake() {
+    console.log(direction);
+}
 
-$(document).on('keyup',function(e){
-    if(e.keyCode == 37){
-        if(direction != 'right'){
-            direction = 'left';
-        }
-    }else if(e.keyCode == 38){
-        if(direction != 'down'){
-            direction = 'up';
-        }
-    }else if(e.keyCode == 39){
-        if(direction != 'left'){
-            direction = 'right';
-        }
-    }else if(e.keyCode == 40){
-        if(direction != 'up'){
-            direction = 'down';
+$(()=>{
+    setAutoPath();
+})
+
+const setAutoPath = () => {
+    const box_main_height = $(".box_main").height();
+    const box_main_width = $(".box_main").width();
+
+    for (let i = 1; i <= 18; i++) {
+        $(".box_main").append(`<div class='row_${i} flex'></div>`);
+        for (let j = 1; j <= 18; j++) {
+            const id = `box_${i}_${j}`;
+            $(`.row_${i}`).append(`<div class='position_box position_box_color' id='${id}'></div>`);
         }
     }
 
-    console.log(direction)
-});
+    randomSankePosition();
+};
+
+
+const randomSankePosition = () => {
+    const random_i = Math.floor(Math.random() * 18) + 1;
+    const random_j = Math.floor(Math.random() * 18) + 1;
+    const id = `#box_${random_i}_${random_j}`;
+    $(id).removeClass('position_box_color').addClass('box_food');
+};
+
+
