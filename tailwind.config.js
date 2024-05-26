0669a77732000b42dbf7e5542e190c19625b2b52
Rayhan Al Shorif <126382033+rayhanalshorif133@users.noreply.github.com>
2024-05-26 11:36:17 +0600

    trans added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,6 @@ module.exports = {
       width:{
         "360": "360px",
       },
-      translate: {
-        '134': '134px',
-        '268': '268px',
-        '402': '402px',
-        '536': '536px',
-      }
     },
   },
   plugins: [],
