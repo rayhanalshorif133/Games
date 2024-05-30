4d26f4eb60f2b92e5c7c5c759675714f5e780920
Rayhan Al Shorif <126382033+rayhanalshorif133@users.noreply.github.com>
2024-05-30 16:21:07 +0600

    upload fly

--- js_game/player.js
+++ js_game/player.js
@@ -1,5 +1,24 @@
-export default class Player {
-    constructor(game){
+export class Player {
+    constructor(game) {
         this.game = game;
+        this.width = 100;
+        this.height = 91.3;
+        this.x = -100;
+        this.y = this.game.height - this.height;
+        this.player = document.getElementById('player');
+       
+    }
+    update() {
+        this.x = this.x + 1;
+        if(this.x > 700){
+            this.x = -100;
+        }
+    }
+    draw(context) {
+        context.fillStyle = 'red';
+
+        context.drawImage(this.player, this.x, this.y, 80, 80 * this.player.height / this.player.width)
+
+
     }
 }
--- js_game/script.js
+++ js_game/script.js
@@ -1,17 +1,49 @@
+import {Player} from './player.js';
+
 window.addEventListener('load', function(){
     const canvas = document.getElementById('canvas_1');
     const ctx = canvas.getContext('2d');
+    const GET_PALYER = document.getElementById('player');
+    var number = 0;
+    var src = '';
 
-    canvas.width = 400;
+    canvas.width = 700;
     canvas.height = 400;
 
     class Game {
         constructor(width, height) {
             this.width = width;
             this.height = height;
+            this.player = new Player(this);
         }
 
-        update() {}
-        draw() {}
+        update() {
+            this.player.update();
+        }
+        draw(context) {
+            this.player.draw(context);
+        }
     }
+
+
+    const game = new Game(canvas.width,canvas.height);
+
+    function animate(){
+        ctx.clearRect(0,0,canvas.width,canvas.height);
+
+        if(number == 14){
+            number = 0;
+        }
+        number++;
+        src = `./images/star_bug_sprites/${number}.png`;
+        GET_PALYER.setAttribute('src',src);
+        
+
+        game.update();
+        game.draw(ctx);
+        requestAnimationFrame(animate);
+    }
+
+    animate();
+
 });
