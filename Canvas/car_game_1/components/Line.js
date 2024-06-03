8a188f3e3826903f4da20c8b8d1f692d2befaa20
Rayhan Al Shorif <126382033+rayhanalshorif133@users.noreply.github.com>
2024-06-03 15:32:03 +0600

    line js

--- Canvas/car_game_1/components/Line.js
+++ Canvas/car_game_1/components/Line.js
@@ -1,15 +1,15 @@
 export class Line {
     constructor(game) {
         this.game = game;
-        this.line_runing_1 = -10;
+        this.line_runing_1 = 0;
         this.line_runing_2 = 150;
     }
 
     update(ctx) { 
         this.line_runing_1 += 10;
         this.line_runing_2 += 10;
-        this.game.height == this.line_runing_1? this.line_runing_1 = -10 : -10;
-        this.game.height == this.line_runing_2? this.line_runing_2 = -10 : -10;
+        if(this.game.height == this.line_runing_1) this.line_runing_1 = -10
+        if(this.game.height == this.line_runing_2) this.line_runing_2 = -50
     }
     draw(ctx) {
         ctx.fillStyle = '#FFFFFF';
