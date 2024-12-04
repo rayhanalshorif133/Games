32f6ea7086fd1a786c753ab98236657d6ecea076
rayhanalshorif133 <rayhanalshorif@gmail.com>
2024-12-04 22:42:00 +0600

    update --- tab to play

---
+++ main/Fortress-Defenders/wave-1/scripts/project/scriptsInEvents.js
@@ -0,0 +1,14 @@
+
+
+
+const scriptsInEvents = {
+
+	async E_game_1_Event25_Act2(runtime, localVars)
+	{
+		document.body.style.cursor = 'none';
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
